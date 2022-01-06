60e94ab294edbd8aa9cbedbc2a0e9c99824d2482
AHITM05 <AHITM05@leomac05.local>
2022-01-06 23:47:13 +0100

    finished jsonDownload

---
+++ dispatch/dispatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dispatch
+//
+//  Created by AHITM05 on 10.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
